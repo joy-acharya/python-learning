c63b5981fb4e707de956ee9c20c3dafe8be87c58
Joy Acharya <joycseuiu@gmail.com>
2025-07-23 16:38:42 +0600

    Create list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+list = ['apple', 'mango', 'banana', 'grapes']
+
+print(list[0])   #apple
+
+print(list[-1])  #grapes
+
+print(list[1:3]) # ['mango','banana']
+
+print(list[1:])  # ['mango', 'banana', 'grapes]
