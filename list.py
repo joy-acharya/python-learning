06547f6cd67b20ff9cdc54a97b79f611b223c3fd
Joy Acharya <joycseuiu@gmail.com>
2025-07-23 16:42:17 +0600

    Update list.py

--- list.py
+++ list.py
@@ -7,3 +7,21 @@ print(list[-1])  #grapes
 print(list[1:3]) # ['mango','banana']
 
 print(list[1:])  # ['mango', 'banana', 'grapes]
+
+
+list = ['apple', 'mango', 'banana', 'grapes']
+
+print(list[0])   #apple
+
+print(list[-1])  #grapes
+
+print(list[1:3]) # ['mango','banana']
+
+print(list[1:])  # ['mango', 'banana', 'grapes]
+
+
+for item in list:
+    print(item)
+
+for item in list[1:]:
+    print(item)
